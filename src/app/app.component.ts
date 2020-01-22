23f6a4ba5804cd058086f98530d2c59bbdbc3c8e
evemula-greenlots <evemula@greenlots.com>
2020-01-22 14:55:39 -0800

    iocns updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ConnectionService } from 'ng-connection-service';
+import { trigger, transition, query, animate, style, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('grow', [
+      transition(':enter', [
+        style({ height: 0 }),
+        animate(300, style({height: '*'}))
+      ]),
+      transition(':leave', [
+        style({ height: '*' }),
+        animate(300, style({height: 0}))
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
+
   title = 'greenlots-fleet-new';
+  connected = true;
 
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private connectionService: ConnectionService
   ) {
     this.matIconRegistry.addSvgIcon(
       "glf-qr",
@@ -22,6 +39,10 @@ export class AppComponent {
       "glf-flash",
       this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/svg/flash.svg")
     );
+
+    this.connectionService.monitor().subscribe(connected => {
+      this.connected = connected;
+    });
   }
 
 }
